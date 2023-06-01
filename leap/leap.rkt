413ff7b31f8fb9accf7f6ae8d761ef2da0443d82
Kim Hallberg <hallberg.kim@gmail.com>
2023-06-01 16:55:33 +0200

    Update `Leap` exercise

--- leap/leap.rkt
+++ leap/leap.rkt
@@ -7,3 +7,9 @@
       true (if (zero? (modulo year 100))
           false (if (zero? (modulo year 4))
             true false))))
+
+; @cnasc inspired alternative
+; (define (leap-year? year)
+;   (and (zero? (modulo year 4))
+;       (or (zero? (modulo year 400))
+;          (not (zero? (modulo year 100))))))
