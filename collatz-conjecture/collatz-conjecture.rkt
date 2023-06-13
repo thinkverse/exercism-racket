6d3b9623f0db486655b90e49e65a12e50437115c
Kim Hallberg <hallberg.kim@gmail.com>
2023-06-13 19:47:51 +0200

    Complete `Collatz Conjecture` exercise

--- collatz-conjecture/collatz-conjecture.rkt
+++ collatz-conjecture/collatz-conjecture.rkt
@@ -3,4 +3,13 @@
 (provide collatz)
 
 (define (collatz num)
-  (error "Not implemented yet"))
+  (if (exact-positive-integer? num)
+      (letrec ([reduce (lambda (n t)
+                         (cond
+                           [(eq? n 1) t]
+                           [(even? n) (reduce (/ n 2) (add1 t))]
+                           [else (reduce (add1 (* n 3)) (add1 t))]
+                           )
+                         )])
+        (reduce num 0))
+      (error "cannot calculate collatz on nonpositive-integer")))
